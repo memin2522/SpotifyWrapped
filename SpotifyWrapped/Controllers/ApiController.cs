4fbf881b5804dabc55c4688236091994f698326c
Memo Reyes <sergio@yeltic.com>
2024-12-05 22:03:49 -0500

    Separate index and api function

--- SpotifyWrapped/Controllers/ApiController.cs
+++ SpotifyWrapped/Controllers/ApiController.cs
@@ -1,19 +1,18 @@
-using ETLYelticDashboard.Classes.Database.Generic;
+﻿using ETLYelticDashboard.Classes.Database.Generic;
 using Microsoft.AspNetCore.Mvc;
 using SpotifyWrapped.Classes;
-using SpotifyWrapped.Models;
-using System.Diagnostics;
 
 namespace SpotifyWrapped.Controllers
 {
-    public class HomeController : Controller
+    [ApiController]
+    [Route("[controller]")]
+    public class ApiController : Controller
     {
         private readonly ILogger<HomeController> _logger;
         private readonly DatabaseIntermediary _dbSQL;
         private readonly LoadInfo _loadInfo;
         private readonly DataExtraction _extractInfo;
-
-        public HomeController(DatabaseIntermediary databaseIntermediary, ILogger<HomeController> logger)
+        public ApiController(DatabaseIntermediary databaseIntermediary, ILogger<HomeController> logger)
         {
             _dbSQL = databaseIntermediary;
             _logger = logger;
@@ -21,9 +20,13 @@ namespace SpotifyWrapped.Controllers
             _extractInfo = new DataExtraction();
         }
 
+        [HttpGet("extract-spotify-data")]
         public async Task<IActionResult> Index()
         {
-            return View();
+            var extractedData = await _extractInfo.ExtractionCicle();
+            await _loadInfo.ProcessRawData(extractedData);
+
+            return Ok("Procesado mi perro");
         }
     }
 }
